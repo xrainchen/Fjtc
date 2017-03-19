4ac7c346bf4a1668031a8f10ab90636d67e8e6e3
xrainchen <xrainchen@163.com>
2017-03-19 22:41:51 +0800

    权限菜单

---
+++ 开发/src/fjtc/admin.fjtc.com/Controllers/CMSPowerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.Mvc;
+using admin.fjtc.com.Common;
+using Fjtc.BLL;
+using Fjtc.Model.SearchModel;
+
+namespace admin.fjtc.com.Controllers
+{
+    /// <summary>
+    /// 权限控制器
+    /// </summary>
+    public class CMSPowerController : BaseController
+    {
+        // GET: CMSPower
+        [HttpGet]
+        public ActionResult List(CMSPowerSearchParameter searchObj)
+        {
+            searchObj.ReturnList = new CMSPowerBll().GetPowerTree();
+            return View(searchObj);
+        }
+        [HttpPost]
+        public ActionResult List(CMSPowerSearchParameter searchObj, FormCollection collection)
+        {
+            try
+            {
+                searchObj.ReturnList = new CMSPowerBll().GetAllPower();
+                searchObj.Count = 1000;
+                return Json(searchObj);
+            }
+            catch (Exception ex)
+            {
+                return DwzHelper.Warn(ex.Message);
+            }
+        }
+    }
+}
