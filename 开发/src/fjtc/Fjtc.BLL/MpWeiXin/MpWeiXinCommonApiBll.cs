09e1236e67e1e1d43b206f8620a0c49491538fbf
xrainchen <xrainchen@163.com>
2017-03-22 23:44:53 +0800

    微信用户接口

---
+++ 开发/src/fjtc/Fjtc.BLL/MpWeiXin/MpWeiXinCommonApiBll.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Fjtc.Model.Entity;
+using Senparc.Weixin.MP.Entities;
+
+namespace Fjtc.BLL.MpWeiXin
+{
+    public class MpWeiXinCommonApiBll
+    {
+        public async Task<AccessTokenResult> GetAccessTokenAsync(MpWeiXinAccessSetting setting)
+        {
+            return await Senparc.Weixin.MP.CommonAPIs.CommonApi.GetTokenAsync(setting.AppId, setting.AppSecret);
+        }
+
+        /// <summary>
+        /// 获取AccessToken  缓存处理 1.5小时更新一次
+        /// </summary>
+        /// <param name="setting"></param>
+        /// <returns></returns>
+        public AccessTokenResult GetAccessToken(MpWeiXinAccessSetting setting)
+        {
+            return Senparc.Weixin.MP.CommonAPIs.CommonApi.GetToken(setting.AppId, setting.AppSecret);
+        }
+
+        public WeixinUserInfoResult GetUserInfo(string accessTokenOrAppid, string nextOpenId="")
+        {
+            return Senparc.Weixin.MP.CommonAPIs.CommonApi.GetUserInfo(accessTokenOrAppid, nextOpenId);
+        }
+        public async Task<WeixinUserInfoResult> GetUserInfoAsync(string accessTokenOrAppid, string nextOpenId = "")
+        {
+            return await Senparc.Weixin.MP.CommonAPIs.CommonApi.GetUserInfoAsync(accessTokenOrAppid, nextOpenId);
+        }
+    }
+}
